a1c918a6aac81cc6a990f064b3d2d93e293a449c
Agrim Mittal <16553016+agrim123@users.noreply.github.com>
2022-02-19 14:23:39 +0530

    (ecr) Add cleanup cmd (#26)

---
+++ cmd/ecr.go
@@ -0,0 +1,39 @@
+package cmd
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	configPkg "github.com/mudrex/onyx/pkg/config"
+	"github.com/mudrex/onyx/pkg/core/ecr"
+	"github.com/spf13/cobra"
+)
+
+var ecrCommand = &cobra.Command{
+	Use:   "ecr",
+	Short: "Actions to be performed on ECR",
+}
+
+var ecrCleanupCommand = &cobra.Command{
+	Use:   "cleanup",
+	Short: "Cleans up older tags pushed on ECR repository",
+	Args:  cobra.ExactArgs(1),
+	PreRunE: func(cmd *cobra.Command, args []string) error {
+		return configPkg.LoadConfig()
+	},
+	Example: "onyx ecr cleanup",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(configPkg.GetRegion()))
+		if err != nil {
+			log.Fatalf("unable to load SDK config, %v", err)
+		}
+		ctx := context.Background()
+
+		return ecr.Cleanup(ctx, cfg, args[0])
+	},
+}
+
+func init() {
+	ecrCommand.AddCommand(ecrCleanupCommand)
+}
